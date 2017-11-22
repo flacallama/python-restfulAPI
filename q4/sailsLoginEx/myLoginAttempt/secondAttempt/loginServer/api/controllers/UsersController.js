949fec91c99720e9cd416b6b63d7ff3e7cd32d68
Dylan Cunningham <gdylanc@gmail.com>
2017-11-21 17:30:34 -0700

    finished route findUserByUsername

--- q4/sailsLoginEx/myLoginAttempt/secondAttempt/loginServer/api/controllers/UsersController.js
+++ q4/sailsLoginEx/myLoginAttempt/secondAttempt/loginServer/api/controllers/UsersController.js
@@ -6,6 +6,26 @@
  */
 
 module.exports = {
-	
-};
 
+
+	findUserByUsername: function(req, res) {
+		var _username = req.params.id
+		return Users.find().where({username: _username})
+		.exec(function (err, _user){
+			if (err) {
+				return res.json({
+					 error: err
+				});
+			}
+			// if (_event == undefined){
+			// 	return res.notFound();
+			// }
+			return res.json({
+				user: _user
+			})
+		})
+	}
+
+
+
+};
